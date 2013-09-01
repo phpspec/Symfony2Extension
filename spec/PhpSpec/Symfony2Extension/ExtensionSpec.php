a6580278206f4f7a711c01fbe7dc334f6c179cf5
Jakub Zalas <jakub@zalas.pl>
2013-09-01 13:47:13 +0100

    Enabled the locator.

--- spec/PhpSpec/Symfony2Extension/ExtensionSpec.php
+++ spec/PhpSpec/Symfony2Extension/ExtensionSpec.php
@@ -3,11 +3,77 @@
 namespace spec\PhpSpec\Symfony2Extension;
 
 use PhpSpec\ObjectBehavior;
+use PhpSpec\ServiceContainer;
+use Prophecy\Argument;
 
 class ExtensionSpec extends ObjectBehavior
 {
+    private $configurator;
+
     function it_is_a_phpspec_extension()
     {
         $this->shouldHaveType('PhpSpec\Extension\ExtensionInterface');
+    }
+
+    function it_adds_a_custom_locator_with_configuration(ServiceContainer $container)
+    {
+        $container->getParam('symfony2_locator')->willReturn(
+            array(
+                'namespace' => 'Acme',
+                'spec_sub_namespace' => 'Specs',
+                'src_path' => 'lib',
+                'spec_paths' => array('lib/Acme/*/Specs')
+            )
+        );
+
+        $container->addConfigurator($this->trackedConfigurator())->shouldBeCalled();
+        $container->setShared(
+            'locator.locators.symfony2_locator',
+            $this->service('PhpSpec\Symfony2Extension\Locator\PSR0Locator', $container)
+        )->shouldBeCalled();
+
+        $this->load($container);
+        $configurator = $this->configurator;
+        $configurator($container->getWrappedObject());
+    }
+
+    private function trackedConfigurator()
+    {
+        $this->configurator = function () { throw new \LogicException('Configurator was not added'); };
+
+        $configurator = &$this->configurator;
+
+        return Argument::that(
+            function ($c) use (&$configurator) {
+                if (!is_callable($c)) {
+                    return false;
+                }
+
+                $configurator = $c;
+
+                return true;
+            }
+        );
+    }
+
+    private function service($class, ServiceContainer $container)
+    {
+        return Argument::that(
+            function ($callback) use ($class, $container) {
+                if (!is_callable($callback)) {
+                    throw new \LogicException('Expected a callable to be set on the container');
+                }
+
+                $result = $callback($container->getWrappedObject());
+
+                if (!$result instanceof $class) {
+                    $message = sprintf('Expected the service to be an instance of "%s" but got: "%s"', $class, is_object($result) ? get_class($result) : gettype($result));
+
+                    throw new \LogicException($message);
+                }
+
+                return true;
+            }
+        );
     }
 }
--- src/PhpSpec/Symfony2Extension/Extension.php
+++ src/PhpSpec/Symfony2Extension/Extension.php
@@ -4,6 +4,7 @@ namespace PhpSpec\Symfony2Extension;
 
 use PhpSpec\Extension\ExtensionInterface;
 use PhpSpec\ServiceContainer;
+use PhpSpec\Symfony2Extension\Locator\PSR0Locator;
 
 class Extension implements ExtensionInterface
 {
@@ -12,5 +13,27 @@ class Extension implements ExtensionInterface
      */
     public function load(ServiceContainer $container)
     {
+        $container->addConfigurator($this->getLocatorConfigurator());
+    }
+
+    /**
+     * @return callable
+     */
+    private function getLocatorConfigurator()
+    {
+        return function ($c) {
+            $suite = $c->getParam('symfony2_locator');
+            $namespace = isset($suite['namespace']) ? $suite['namespace'] : '';
+            $specSubNamespace = isset($suite['spec_sub_namespace']) ? $suite['spec_sub_namespace'] : 'Spec';
+            $srcPath = isset($suite['src_path']) ? $suite['src_path'] : 'src';
+            $specPaths = isset($suite['spec_paths']) ? $suite['spec_paths'] : array('src/*/Bundle/*Bundle/Spec', 'src/*/*/Spec');
+
+            $c->setShared('locator.locators.symfony2_locator',
+                function($c) use ($namespace, $specSubNamespace, $srcPath, $specPaths) {
+                    return new PSR0Locator($namespace, $specSubNamespace, $srcPath, $specPaths);
+                }
+            );
+        };
+
     }
 }
